8be94d3524ba497b06bff3cbe2cf874a44defa64
Marc LeGendre <marc@instructure.com>
2013-10-24 11:38:32 -0600

    Did some major tweaking

--- Gemfile
+++ Gemfile
@@ -1,18 +1,24 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
+gem 'rubyzip', '< 1.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
-
+gem 'bootstrap-sass', '2.3.2.0'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 # Use SCSS for stylesheets
 group :test do
   gem 'selenium-webdriver','2.0.0'
   gem 'capybara', '2.1.0'
+
 end
 
 gem 'sass-rails', '~> 4.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,50 @@
 require 'spec_helper'
 
-describe "Static Pages" do
+describe "Static pages" do
+  subject { page }
+  describe "Home page" do
+    before { visit root_path}
 
-  describe "Home Page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-  
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
   end
 
-  describe "Help Page" do
-    
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
-  describe "About Page" do
-    
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
-end 
- 
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+end
